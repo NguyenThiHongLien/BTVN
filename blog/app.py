5013e14b76166647da21985686367e10c4ff83f8
NguyenThiHongLien <Honglien94.pt@gmail.com>
2021-11-21 11:07:01 +0700

    blog

--- blog/app.py
+++ blog/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 from os import error
+from docx import Document
 
 
 app = Flask(__name__)
@@ -69,17 +70,18 @@ def insert():
 @app.route("/<id>", methods = ['GET'])
 def edit(id):
     cur = mysql.connection.cursor()
-    cur.execute("SELECT  * FROM blogs WHERE id = %s", id)
+    cur.execute("SELECT  * FROM blogs WHERE id = %s", [id])
     post = cur.fetchone()
     cur.close()
     
     return render_template("edit.html", post =post)
 
 @app.route('/<id>',methods=['POST'])
-def update():
-
+def update(id):
+    
     if request.method == 'POST':
-        id_data = request.form['id']
+        #id_data = request.form['id']
+        id_data = ["id"]
         title = request.form['title']
         content = request.form['content']
         cur = mysql.connection.cursor()
@@ -90,7 +92,69 @@ def update():
             """, (title, content, id_data))
         flash("Data Updated Successfully")
         mysql.connection.commit()
+        
         return redirect(url_for('Index'))
+
+
+@app.route("/post/<post_id>", methods=["POST"])
+def edit_post(post_id):
+    title = request.form.get("title")
+    content = request.form.get("content")
+
+    if title and content:
+        sql = '''
+            SELECT * FROM blogs
+            WHERE id = %s
+        '''
+        cur = mysql.connection.cursor()
+        cur.execute(sql, [post_id])
+        post = cur.fetchone()
+
+        if not post:
+            flash("Post does not exists")
+        else:
+            sql = '''
+                UPDATE blogs
+                SET title = %s,
+                    content = %s
+                WHERE
+                    id = %s
+            '''
+
+            cur.execute(sql, (title, content, post_id))
+            mysql.connection.commit()
+            flash("Post updated!")
+
+        return redirect(url_for('Index'))
+    else:
+        flash("Title and content cannot be empty")
+        return redirect(url_for('Index'))
+
+@app.route("/resignation-letter", methods=["GET"])
+def render_form():
+    return render_template("resignation.html")
+
+
+
+@app.route("/resignation-letter", methods=["POST"])
+def create_file():
+    from pathlib import Path
+    from docx import Document
+    from docx.shared import Cm
+    fullname = request.form.get("fullname")
+    reason = request.form.get("reason")
+
+    if fullname and reason:
+        doc = Document()
+        doc.add_heading("Resignation Letter", level=0)
+        doc.add_paragraph(fullname)
+        doc.add_paragraph(reason)
+        file_name = 'resignation-letter.docx'
+        doc.save(str(Path(__file__).parent.absolute())+'/static/resignation-letter.docx')
+        return render_template("resignation.html", file_created=True)
+    else:
+        flash("Fullname and reason cannot be empty")
+        return render_template("resignation.html", file_created=False)
 
 if __name__ == "__main__":
     app.run(debug=True)
